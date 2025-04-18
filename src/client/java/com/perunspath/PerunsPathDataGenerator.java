8da605533e7ffb2bd2cc5bd0216149db1670899f
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2025-04-18 19:28:23 +0700

    First commit

---
+++ src/client/java/com/perunspath/PerunsPathDataGenerator.java
@@ -0,0 +1,11 @@
+package com.perunspath;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PerunsPathDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
